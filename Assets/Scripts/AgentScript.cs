9b0f7c20083b776fa59d560d1c1e63fab19b078a
CG-F16-24-Rutgers <kjm263@scarletmail.rutgers.edu>
2016-09-28 16:01:30 -0400

    Director script added

--- Assets/Scripts/AgentScript.cs
+++ Assets/Scripts/AgentScript.cs
@@ -1,28 +1,32 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class AgentScript : MonoBehaviour
-{
+public class AgentScript : MonoBehaviour {
+	private bool selected;
+	private float greenTime;
+	NavMeshAgent agent;
 
-     NavMeshAgent agent;
-
-     // Use this for initialization
-     void Start()
-     {
-         agent = GetComponent<NavMeshAgent>();
-     }
-
-     // Update is called once per frame
-     void Update()
-     {
-         if (Input.GetMouseButtonDown(0))
-         {
-             RaycastHit hit;
-
-             if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 100))
-             {
-                 agent.destination = hit.point;
-             }
-         }
-     }
+	void Start() {
+		agent = GetComponent<NavMeshAgent>();
+		selected = false;
+		greenTime = 0.0f;
+	}
+	void Update() {
+		if (selected) {
+			greenTime += Time.deltaTime;
+			float rb = 1.0f - Mathf.Abs(Mathf.Sin(greenTime * 3.0f));
+			GetComponent<Renderer>().material.SetColor("_Color", new Vector4(rb, 1.0f, rb, 1.0f));
+		}
+	}
+	public void setSelected() {
+		selected = !selected;
+		GetComponent<Renderer>().material.SetColor("_Color", Color.white);
+	}
+	public void moveTo(RaycastHit hit) {
+		if (selected) {
+			GetComponent<Renderer>().material.SetColor("_Color", Color.white);
+			selected = false;
+			agent.destination = hit.point;
+		}
+	}
 }
---
+++ Assets/Scripts/Director.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Director : MonoBehaviour {
+	void Start() {
+	}
+	void Update() {
+		if (Input.GetMouseButtonDown(0)) {
+			RaycastHit hit;
+			if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 100)) {
+				if (hit.transform.tag.Equals("Player")) {
+					hit.transform.gameObject.GetComponent<AgentScript>().setSelected();
+				} else {
+					GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
+					foreach (GameObject p in players) {
+						p.GetComponent<AgentScript>().moveTo(hit);
+					}
+				}
+			}
+		}
+	}
+}
