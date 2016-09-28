31bc0680c5c6f594d5273f309c80170e1612147a
CG-F16-24-Rutgers <kjm263@scarletmail.rutgers.edu>
2016-09-28 15:01:04 -0400

    added moving obstacles

---
+++ Assets/Scripts/Obstacle.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Obstacle : MonoBehaviour {
+	bool movingRight;
+	float moveTimer;
+	float speed = 5.0f;
+	void Start() {
+		moveTimer = 0.0f;
+		movingRight = false;
+	}
+	void Update() {
+		if (movingRight) {
+			transform.Translate(Vector3.right * speed * Time.deltaTime);
+		} else {
+			transform.Translate(Vector3.left * speed * Time.deltaTime);
+		}
+		moveTimer += speed * Time.deltaTime;
+		if (moveTimer >= 20.0f) {
+			movingRight = !movingRight;
+			moveTimer = 0.0f;
+		}
+	}
+}
