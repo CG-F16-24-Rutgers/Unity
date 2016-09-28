3c4fc0f6ef89aca643e8d56fc333043b2ff17217
Hasan Mehmedagic <hm356@scarletmail.rutgers.edu>
2016-09-27 20:33:28 -0400

    free look camera

--- Assets/Scripts/AgentScript.cs
+++ Assets/Scripts/AgentScript.cs
@@ -1,18 +1,28 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class AgentScript : MonoBehaviour {
+public class AgentScript : MonoBehaviour
+{
 
-    public Transform target;
-    NavMeshAgent agent;
+     NavMeshAgent agent;
 
-	// Use this for initialization
-	void Start () {
-        agent = GetComponent<NavMeshAgent>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        agent.SetDestination(target.position);
-	}
+     // Use this for initialization
+     void Start()
+     {
+         agent = GetComponent<NavMeshAgent>();
+     }
+
+     // Update is called once per frame
+     void Update()
+     {
+         if (Input.GetMouseButtonDown(0))
+         {
+             RaycastHit hit;
+
+             if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 100))
+             {
+                 agent.destination = hit.point;
+             }
+         }
+     }
 }
