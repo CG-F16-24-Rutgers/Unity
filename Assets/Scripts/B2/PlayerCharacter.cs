d66a8b4ad0b8c61959769ed99596fcb5f1d8d7da
CG-F16-24-Rutgers <kjm263@scarletmail.rutgers.edu>
2016-10-11 02:13:21 -0400

    started project

---
+++ Assets/Scripts/B2/PlayerCharacter.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerCharacter : MonoBehaviour {
+	void Start() {
+	}
+	void Update() {
+		if (Input.GetKey("up")) {
+			transform.position += transform.forward * Time.deltaTime * 4.0f;
+		}
+		if (Input.GetKey("down")) {
+			transform.position -= transform.forward * Time.deltaTime * 4.0f;
+		}
+		if (Input.GetKey("left")) {
+			transform.Rotate(Vector3.up * Time.deltaTime * -64.0f);
+		}
+		if (Input.GetKey("right")) {
+			transform.Rotate(Vector3.up * Time.deltaTime * 64.0f);
+		}
+	}
+}
---
+++ Assets/Scripts/B2/ThirdPersonCam.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ThirdPersonCam : MonoBehaviour {
+	public GameObject player;
+	void Start() {
+		transform.SetParent(player.transform);
+	}
+}
