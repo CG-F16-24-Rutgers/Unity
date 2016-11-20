af1568e2a0de3cd821ea6348142298529472d91b
Hasan Mehmedagic <hm356@scarletmail.rutgers.edu>
2016-11-20 12:17:22 -0500

    Revert "Revert "camera added""
    This reverts commit c598892a680badf6c47b069101fcbc7086154d20.

---
+++ Assets/Scripts/B4/B4Camera.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class B4Camera : MonoBehaviour {
+	public GameObject player;
+	private Vector3 offset;
+	void Start () {
+		offset = player.transform.position - transform.position;
+	}
+	void Update () {
+	}
+	void LateUpdate() {
+		transform.position = player.transform.position - offset;
+	}
+}
