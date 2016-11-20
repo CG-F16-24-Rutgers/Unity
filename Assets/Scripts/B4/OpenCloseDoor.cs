147382f9ce7d2051a23a7561facc7639bcc27730
Hasan Mehmedagic <hm356@scarletmail.rutgers.edu>
2016-11-19 19:05:03 -0500

    attempt to get character moving

---
+++ Assets/Scripts/B4/OpenCloseDoor.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OpenCloseDoor : MonoBehaviour {
+
+    // Smothly open a door
+    float smooth = 2.0f;
+    float DoorOpenAngle = 90.0f;
+    public bool open;
+    private bool enter;
+
+    private Vector3 defaultRot;
+    private Vector3 openRot;
+
+    void Start()
+    {
+        defaultRot = transform.eulerAngles;
+        openRot = new Vector3(defaultRot.x, defaultRot.y + DoorOpenAngle, defaultRot.z);
+    }
+
+    //Main function
+    void Update()
+    {
+        if (open)
+        {
+            //Open door
+            transform.eulerAngles = Vector3.Slerp(transform.eulerAngles, openRot, Time.deltaTime * smooth);
+        }
+        else
+        {
+            //Close door
+            transform.eulerAngles = Vector3.Slerp(transform.eulerAngles, defaultRot, Time.deltaTime * smooth);
+        }
+
+        if (Input.GetKeyDown("f") && enter)
+        {
+            open = !open;
+        }
+    }
+
+    //Activate the Main function when player is near the door
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            enter = true;
+        }
+    }
+
+    //Deactivate the Main function when player is go away from door
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            enter = false;
+        }
+    }
+}
