b2aad1aaca38fd0fd843a19d314c010b79f176e5
CG-F16-24-Rutgers <kjm263@scarletmail.rutgers.edu>
2016-11-19 19:06:59 -0500

    back up commit

---
+++ Assets/Scripts/B4/Patrol.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Patrol : MonoBehaviour {
+	public Transform[] waypoints;
+	private NavMeshAgent agent;
+	private int nextPoint;
+	void Start () {
+		agent = GetComponent<NavMeshAgent>();
+		nextPoint = 0;
+		agent.destination = waypoints[0].position;
+	}
+	void Update () {
+		Collider[] 
+		if (agent.remainingDistance < 0.5f) {
+			nextPoint++;
+			agent.destination = waypoints[nextPoint % waypoints.Length].position;
+		}
+	}
+}
