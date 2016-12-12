ec63ead83573972486eacf78b729628d75ae23a1
Hasan Mehmedagic <hm356@scarletmail.rutgers.edu>
2016-12-12 00:02:01 -0500

    Wall Climbing added

---
+++ Assets/Scripts/B5/IKSnap.cs
@@ -0,0 +1,233 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IKSnap : MonoBehaviour {
+
+    public bool use_IK = true;
+
+    public bool left_hand_IK = false;
+    public bool right_hand_IK = false;
+
+    public bool left_foot_IK = false;
+    public bool right_foot_IK = false;
+
+    private Vector3 left_hand_pos;
+    private Vector3 right_hand_pos;
+
+    private Vector3 left_foot_pos;
+    private Vector3 right_foot_pos;
+
+    private Vector3 left_hand_opos;
+    private Vector3 right_hand_opos;
+
+    public Vector3 left_hand_offset;
+    public Vector3 right_hand_offset;
+
+    public Vector3 left_foot_offset;
+    public Vector3 right_foot_offset;
+
+    private Quaternion left_hand_rot;
+    private Quaternion right_hand_rot;
+
+    private Quaternion left_foot_rot;
+    private Quaternion right_foot_rot;
+
+    public Quaternion left_foot_rot_offset;
+    public Quaternion right_foot_rot_offset;
+
+    public bool useFootIK = false;
+
+    private Animator anim;
+    private float normalized_time;
+
+    public float lh_weight = 1f;
+    public float rh_weight = 1f;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (use_IK)
+        {
+            anim.SetBool("useIK", true);
+        }
+        else {
+            anim.SetBool("useIK", false);
+        }
+
+        RaycastHit lh_hit;
+        RaycastHit rh_hit;
+
+        RaycastHit lf_hit;
+        RaycastHit rf_hit;
+
+        //Left Hand IK Check
+        if (Physics.Raycast(transform.position + transform.TransformDirection(new Vector3(0.0f, 2.0f, 1.0f)), transform.TransformDirection(new Vector3(-0.5f, -1.0f, 0.0f)), out lh_hit, 1f))
+        {
+
+            Vector3 lookAt = Vector3.Cross(-lh_hit.normal, transform.right);
+            lookAt = lookAt.y < 0 ? -lookAt : lookAt;
+
+            //Setting true if raycast hits something
+            left_hand_IK = true;
+
+            //Setting left_hand_pos to raycast hit points and subtracting the offsets
+            left_hand_pos = lh_hit.point - transform.TransformDirection(left_hand_offset);
+            //left_hand_rot = Quaternion.FromToRotation(Vector3.forward, lh_hit.normal);
+            left_hand_rot = Quaternion.LookRotation(lh_hit.point + lookAt, lh_hit.normal);
+        }
+        else
+            left_hand_IK = false;
+
+        //Right Hand IK Check
+        if (Physics.Raycast(transform.position + transform.TransformDirection(new Vector3(0.0f, 2.0f, 1.0f)), transform.TransformDirection(new Vector3(0.5f, -1.0f, 0.0f)), out rh_hit, 1f))
+        {
+
+            Vector3 lookAt = Vector3.Cross(-rh_hit.normal, transform.right);
+            lookAt = lookAt.y < 0 ? -lookAt : lookAt;
+
+            //Setting true if raycast hits something
+            right_hand_IK = true;
+
+            //Setting right_hand_pos to raycast hit points and subtracting the offsets
+            right_hand_pos = rh_hit.point - transform.TransformDirection(right_hand_offset);
+            //right_hand_rot = Quaternion.FromToRotation(Vector3.forward, rh_hit.normal);
+            right_hand_rot = Quaternion.LookRotation(rh_hit.point + lookAt, rh_hit.normal);
+        }
+        else
+            right_hand_IK = false;
+
+        if (useFootIK)
+        {
+            //Left Foot IK Check
+            if (Physics.Raycast(transform.position + transform.TransformDirection(new Vector3(-0.35f, 0.5f, 0.0f)), transform.forward, out lf_hit, 1f))
+            {
+
+                left_foot_IK = true;
+                left_foot_pos = lf_hit.point - left_foot_offset;
+                left_foot_rot = (Quaternion.FromToRotation(Vector3.up, lf_hit.normal)) * left_foot_rot_offset;
+            }
+            else
+                left_foot_IK = false;
+
+            //Right Foot IK Check
+            if (Physics.Raycast(transform.position + transform.TransformDirection(new Vector3(0.35f, 0.5f, 0.0f)), transform.forward, out rf_hit, 1f))
+            {
+
+                right_foot_IK = true;
+                right_foot_pos = rf_hit.point - right_foot_offset;
+                right_foot_rot = (Quaternion.FromToRotation(Vector3.up, rf_hit.normal)) * right_foot_rot_offset;
+            }
+            else
+                right_foot_IK = false;
+        }
+        normalized_time = anim.GetCurrentAnimatorStateInfo(0).normalizedTime % 1;
+
+        if (anim)
+        {
+
+            if (anim.GetCurrentAnimatorStateInfo(0).IsName("IdlePose"))
+            { //Change the name to whatever your "ON LEDGE MOVING" animation is named to...
+
+                float vel = 0.0f;
+
+                if (normalized_time < 0.25f)
+                {
+
+                    //Smoothly changing IK Weights from current value to 0 or current value to 1 based on current hand's position (find the specific time based on animation)
+                    lh_weight = Mathf.SmoothDamp(lh_weight, 0.0f, ref vel, 2 * Time.deltaTime);
+                    rh_weight = Mathf.SmoothDamp(rh_weight, 1.0f, ref vel, 8 * Time.deltaTime);
+                }
+
+                else if (normalized_time > 0.25f && normalized_time < 0.5f)
+                {
+
+                    //Smoothly changing IK Weights from current value to 0 or current value to 1 based on current hand's position (find the specific time based on animation)
+                    lh_weight = Mathf.SmoothDamp(lh_weight, 1.0f, ref vel, 8 * Time.deltaTime);
+                    rh_weight = Mathf.SmoothDamp(rh_weight, 0.0f, ref vel, 2 * Time.deltaTime);
+                }
+            }
+
+            else
+            { //Resets the hand weights back to 1.0f, add further animation info here to make sure that weights are 0.0f when in normal player movement
+
+                lh_weight = 1.0f;
+                rh_weight = 1.0f;
+            }
+        }
+    }
+
+    void OnDrawGizmos()
+    {
+
+        //Left Hand IK Visual Ray
+        Debug.DrawRay(transform.position + transform.TransformDirection(new Vector3(0.0f, 2.0f, 1.0f)), transform.TransformDirection(new Vector3(-0.5f, -1.0f, 0.0f)), Color.green);
+
+        //Right Hand IK Visual Ray
+        Debug.DrawRay(transform.position + transform.TransformDirection(new Vector3(0.0f, 2.0f, 1.0f)), transform.TransformDirection(new Vector3(0.5f, -1.0f, 0.0f)), Color.green);
+
+        //Left Foot IK Visual Ray
+        Debug.DrawRay(transform.position + transform.TransformDirection(new Vector3(-0.5f, 0.5f, 0.0f)), transform.forward, Color.red);
+
+        //Right Foot IK Visual Ray
+        Debug.DrawRay(transform.position + transform.TransformDirection(new Vector3(0.5f, 0.5f, 0.0f)), transform.forward, Color.red);
+    }
+
+    void OnAnimatorIK()
+    {
+
+        //Setting up IK Weights and positions
+        if (use_IK)
+        {
+
+            left_hand_opos = anim.GetIKPosition(AvatarIKGoal.LeftHand);
+            right_hand_opos = anim.GetIKPosition(AvatarIKGoal.RightHand);
+
+            if (left_hand_IK)
+            {
+
+                anim.SetIKPositionWeight(AvatarIKGoal.LeftHand, lh_weight);
+                anim.SetIKPosition(AvatarIKGoal.LeftHand, left_hand_pos);
+
+                anim.SetIKRotationWeight(AvatarIKGoal.LeftHand, 1f);
+                anim.SetIKRotation(AvatarIKGoal.LeftHand, left_hand_rot);
+            }
+
+            if (right_hand_IK)
+            {
+
+                anim.SetIKPositionWeight(AvatarIKGoal.RightHand, rh_weight);
+                anim.SetIKPosition(AvatarIKGoal.RightHand, right_hand_pos);
+
+                anim.SetIKRotationWeight(AvatarIKGoal.RightHand, 1f);
+                anim.SetIKRotation(AvatarIKGoal.RightHand, right_hand_rot);
+            }
+
+            if (left_foot_IK)
+            {
+
+                anim.SetIKPositionWeight(AvatarIKGoal.LeftFoot, 1f);
+                anim.SetIKPosition(AvatarIKGoal.LeftFoot, left_foot_pos);
+
+                anim.SetIKRotationWeight(AvatarIKGoal.LeftFoot, 1f);
+                anim.SetIKRotation(AvatarIKGoal.LeftFoot, left_foot_rot);
+            }
+
+            if (right_foot_IK)
+            {
+
+                anim.SetIKPositionWeight(AvatarIKGoal.RightFoot, 1f);
+                anim.SetIKPosition(AvatarIKGoal.RightFoot, right_foot_pos);
+
+                anim.SetIKRotationWeight(AvatarIKGoal.RightFoot, 1f);
+                anim.SetIKRotation(AvatarIKGoal.RightFoot, right_foot_rot);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/B5/WallClimber.cs
@@ -0,0 +1,376 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.Characters.ThirdPerson;
+
+public class WallClimber : MonoBehaviour {
+
+    public float ClimbForce;
+    public float smallestEdge = 0.25f;
+    public float CoolDown = 0.15f;
+    public float minDistance;
+    public float MaxAngle = 30.0f;
+    public float ClimbRange = 2.0f;
+    public float JumpForce = 1.0f;
+    public Climbingsort currentSort;
+    public ThirdPersonUserControl TPUC;
+    public ThirdPersonCharacter TPC;
+
+    public Transform HandTransform;
+    public Vector3 VerticalHandOffset;
+    public Vector3 HorizontalHandOffset;
+    public Vector3 FallHandsOffset;
+    public Vector3 RaycastPosition;
+
+    public LayerMask SpotLayer;
+    public LayerMask CurrentSpotLayer;
+    public LayerMask CheckLayersForObstacle;
+    public LayerMask CheckLayersReachable;
+
+    private Rigidbody rb;
+    private Animator anim;
+
+    private Vector3 TargetPoint;
+    private Vector3 TargetNormal;
+
+
+    private float lasttime;
+    private float BeginDistance;
+    private RaycastHit hit;
+    private Quaternion oldRotation;
+
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+        anim = GetComponent<Animator>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (currentSort == Climbingsort.Walking && Input.GetAxis("Vertical") > 0)
+            StartClimbing();
+
+        if (currentSort == Climbingsort.Climbing)
+            Climb();
+
+        UpdateStatis();
+
+        if (currentSort == Climbingsort.ClimbingTowardsPoint || currentSort == Climbingsort.ClimbingTowardsPlateau)
+            MoveTowardsPoint();
+
+        if (currentSort == Climbingsort.Jumping || currentSort == Climbingsort.Falling)
+            Jumping();
+	}
+
+    public void UpdateStatis() {
+        if (currentSort != Climbingsort.Walking  &&  TPC.m_IsGrounded && currentSort != Climbingsort.ClimbingTowardsPoint) {
+            currentSort = Climbingsort.Walking;
+            TPUC.enabled = true;
+            rb.isKinematic = false;
+        }
+
+        if (currentSort == Climbingsort.Walking && !TPC.m_IsGrounded)
+            currentSort = Climbingsort.Jumping;
+
+        if(currentSort == Climbingsort.Walking && (Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Horizontal") != 0))
+            CheckForClimbStart();
+
+    }
+
+    public void StartClimbing() {
+        if (Physics.Raycast(transform.position + transform.rotation * RaycastPosition, transform.forward, 0.4f) && Time.time - lasttime > CoolDown && currentSort == Climbingsort.Walking) {
+            if (currentSort == Climbingsort.Walking) {
+                rb.AddForce(transform.up * JumpForce);
+            }
+
+            lasttime = Time.time;
+        }
+    }
+
+    public void Jumping() {
+        if (rb.velocity.y < 0 && currentSort != Climbingsort.Falling) {
+            currentSort = Climbingsort.Falling;
+            oldRotation = transform.rotation;
+        }
+
+        if (rb.velocity.y > 0 && currentSort != Climbingsort.Jumping) {
+            currentSort = Climbingsort.Jumping;
+        }
+
+        if (currentSort == Climbingsort.Jumping) 
+            CheckForSpots(HandTransform.position + FallHandsOffset, -transform.up, 0.1f, Checkingsort.normal);
+
+        if (currentSort == Climbingsort.Falling) {
+            CheckForSpots(HandTransform.position + FallHandsOffset + transform.rotation * new Vector3(0.02f, -0.6f, 0.0f), -transform.up, 0.4f, Checkingsort.normal);
+            transform.rotation = oldRotation;
+        }
+    }
+
+    public void Climb() {
+        if (Time.time - lasttime > CoolDown && currentSort == Climbingsort.Climbing)
+        {
+            if (Input.GetAxis("Vertical") > 0)
+            {
+                CheckForSpots(HandTransform.position + transform.rotation * VerticalHandOffset + transform.up * ClimbRange, -transform.up, ClimbRange, Checkingsort.normal);
+
+                if (currentSort != Climbingsort.ClimbingTowardsPoint)
+                    CheckForPlateau();
+            }
+
+            if (Input.GetAxis("Vertical") < 0)
+            {
+                CheckForSpots(HandTransform.position - transform.rotation * (VerticalHandOffset + new Vector3(0.0f, 0.3f, 0.0f)), -transform.up, ClimbRange, Checkingsort.normal);
+
+                if (currentSort != Climbingsort.ClimbingTowardsPoint)
+                {
+                    rb.isKinematic = false;
+                    TPUC.enabled = true;
+                    currentSort = Climbingsort.Falling;
+                    oldRotation = transform.rotation;
+                }
+            }
+
+            if (Input.GetAxis("Horizontal") != 0)
+            {
+                CheckForSpots(HandTransform.position + transform.rotation * HorizontalHandOffset, transform.right * Input.GetAxis("Horizontal") - transform.up / 3.5f, ClimbRange / 2.0f, Checkingsort.normal);
+
+                if (currentSort != Climbingsort.ClimbingTowardsPoint)
+                {
+                    CheckForSpots(HandTransform.position + transform.rotation * HorizontalHandOffset, transform.right * Input.GetAxis("Horizontal") - transform.up / 1.5f, ClimbRange / 3.0f, Checkingsort.normal);
+                }
+
+                if (currentSort != Climbingsort.ClimbingTowardsPoint)
+                {
+                    CheckForSpots(HandTransform.position + transform.rotation * HorizontalHandOffset, transform.right * Input.GetAxis("Horizontal") - transform.up / 6.0f, ClimbRange / 1.5f, Checkingsort.normal);
+                }
+
+                if (currentSort != Climbingsort.ClimbingTowardsPoint)
+                {
+                    int hor = 0;
+
+                    if (Input.GetAxis("Horizontal") < 0)
+                        hor = -1;
+                    if (Input.GetAxis("Horizontal") > 0)
+                        hor = 1;
+
+                    CheckForSpots(HandTransform.position + transform.rotation * HorizontalHandOffset + transform.right * hor * smallestEdge / 4, transform.forward - transform.up * 2, ClimbRange / 3.0f, Checkingsort.turning);
+
+                    if (currentSort != Climbingsort.ClimbingTowardsPoint)
+                        CheckForSpots(HandTransform.position + transform.rotation * HorizontalHandOffset + transform.right * 0.2f, transform.forward - transform.up * 2 + transform.right * hor / 1.5f, ClimbRange / 3.0f, Checkingsort.turning);
+                }
+            }
+        }
+    }
+
+    public void CheckForSpots(Vector3 Spotlocation, Vector3 dir, float range, Checkingsort sort) {
+        bool foundspot = false;
+
+        if (Physics.Raycast(Spotlocation - transform.right * smallestEdge / 2, dir, out hit, range, SpotLayer)) {
+            if (Vector3.Distance(HandTransform.position, hit.point) > minDistance) {
+                foundspot = true;
+
+                FindSpot(hit, sort);
+            }
+        }
+
+        if (!foundspot)
+        {
+            if (Physics.Raycast(Spotlocation + transform.right * smallestEdge / 2, dir, out hit, range, SpotLayer))
+            {
+                if (Vector3.Distance(HandTransform.position, hit.point) > minDistance)
+                {
+                    foundspot = true;
+
+                    FindSpot(hit, sort);
+                }
+            }
+        }
+
+        if (!foundspot) {
+            if (Physics.Raycast(Spotlocation + transform.right * smallestEdge / 2 + transform.forward * smallestEdge, dir, out hit, range, SpotLayer))
+            {
+                if (Vector3.Distance(HandTransform.position, hit.point) - smallestEdge / 1.5f > minDistance)
+                {
+                    foundspot = true;
+
+                    FindSpot(hit, sort);
+                }
+            }
+        }
+
+        if (!foundspot)
+        {
+            if (Physics.Raycast(Spotlocation - transform.right * smallestEdge / 2 + transform.forward * smallestEdge, dir, out hit, range, SpotLayer))
+            {
+                if (Vector3.Distance(HandTransform.position, hit.point) - smallestEdge / 1.5f > minDistance)
+                {
+                    foundspot = true;
+
+                    FindSpot(hit, sort);
+                }
+            }
+        }
+    }
+
+    public void FindSpot(RaycastHit h, Checkingsort sort) {
+        if (Vector3.Angle(h.normal, Vector3.up) < MaxAngle) {
+            RayInfo ray = new RayInfo();
+
+            if (sort == Checkingsort.normal)
+                ray = GetClosestPoint(h.transform, h.point + new Vector3(0.0f, -0.01f, 0.0f), transform.forward / 2.5f);
+            else if (sort == Checkingsort.turning)
+                ray = GetClosestPoint(h.transform, h.point + new Vector3(0.0f, -0.01f, 0.0f), transform.forward / 2.5f - transform.right * Input.GetAxis("Horizontal"));
+            else if (sort == Checkingsort.falling)
+                ray = GetClosestPoint(h.transform, h.point + new Vector3(0.0f, -0.01f, 0.0f), -transform.forward / 2.5f);
+
+            TargetPoint = ray.point;
+            TargetNormal = ray.normal;
+
+            if(ray.CanGoToPoint) {
+                if (currentSort != Climbingsort.Climbing && currentSort != Climbingsort.ClimbingTowardsPoint) {
+                    TPUC.enabled = false;
+                    rb.isKinematic = true;
+                    TPC.m_IsGrounded = false;
+                }
+
+                currentSort = Climbingsort.ClimbingTowardsPoint;
+
+                BeginDistance = Vector3.Distance(transform.position, (TargetPoint - transform.rotation * HandTransform.localPosition));
+            }
+        }
+    }
+
+    public RayInfo GetClosestPoint(Transform trans, Vector3 pos, Vector3 dir) {
+        RayInfo curray = new RayInfo();
+        RaycastHit hit2;
+
+        int oldlayer = trans.gameObject.layer;
+
+        //Change this
+        trans.gameObject.layer = 14;
+
+        if(Physics.Raycast(pos - dir, dir, out hit2, dir.magnitude * 2, CurrentSpotLayer)) {
+            curray.point = hit2.point;
+            curray.normal = hit2.normal;
+
+            if (!Physics.Linecast(HandTransform.position + transform.rotation * new Vector3(0.0f, 0.05f, -0.05f), curray.point + new Vector3(0.0f, 0.5f, 0.0f), out hit2, CheckLayersReachable))
+            {
+                if (!Physics.Linecast(curray.point - Quaternion.Euler(new Vector3(0.0f, 90.0f, 0.0f)) * curray.normal * 0.35f + 0.1f * curray.normal, curray.point + Quaternion.Euler(new Vector3(0.0f, 90.0f, 0.0f)) * curray.normal * 0.35f + 0.1f * curray.normal, out hit2, CheckLayersForObstacle))
+                {
+                    if (!Physics.Linecast(curray.point + Quaternion.Euler(new Vector3(0.0f, 90.0f, 0.0f)) * curray.normal * 0.35f + 0.1f * curray.normal, curray.point - Quaternion.Euler(new Vector3(0.0f, 90.0f, 0.0f)) * curray.normal * 0.35f + 0.1f * curray.normal, out hit2, CheckLayersForObstacle))
+                    {
+                        curray.CanGoToPoint = true;
+                    } else {
+                        curray.CanGoToPoint = false;
+                    }
+                } else {
+                    curray.CanGoToPoint = false;
+                }
+            } else {
+                curray.CanGoToPoint = false;
+            }
+
+            trans.gameObject.layer = oldlayer;
+            return curray;
+
+        } else {
+            curray.CanGoToPoint = false;
+            trans.gameObject.layer = oldlayer;
+            return curray;
+        }
+    }
+
+    public void MoveTowardsPoint() {
+        transform.position = Vector3.Lerp(transform.position, (TargetPoint - transform.rotation * HandTransform.localPosition), Time.deltaTime * ClimbForce);
+
+        Quaternion lookrotation = Quaternion.LookRotation(-TargetNormal);
+
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookrotation, Time.deltaTime * ClimbForce);
+
+        anim.SetBool("OnGround", false);
+
+        float distance = Vector3.Distance(transform.position, (TargetPoint - transform.rotation * HandTransform.localPosition));
+        float percent = -9 * (BeginDistance - distance) / BeginDistance;
+
+        anim.SetFloat("Jump", percent);
+
+        if (distance <= 0.01f && currentSort == Climbingsort.ClimbingTowardsPoint) {
+            transform.position = TargetPoint - transform.rotation * HandTransform.localPosition;
+            transform.rotation = lookrotation;
+
+            lasttime = Time.time;
+            currentSort = Climbingsort.Climbing;
+        }
+
+        if (distance <= 0.25f && currentSort == Climbingsort.ClimbingTowardsPlateau)
+        {
+            transform.position = TargetPoint - transform.rotation * HandTransform.localPosition;
+            transform.rotation = lookrotation;
+
+            lasttime = Time.time;
+            currentSort = Climbingsort.Walking;
+
+            rb.isKinematic = false;
+            TPUC.enabled = true;
+
+        }
+    }
+
+    public void CheckForClimbStart() {
+        RaycastHit hit2;
+
+        Vector3 dir = transform.forward - transform.up / 0.8f;
+
+        if (!Physics.Raycast(transform.position + transform.rotation * RaycastPosition, dir, 1.6f) && !Input.GetButton("Jump")) {
+            currentSort = Climbingsort.CheckingForClimbStart;
+            if (Physics.Raycast(transform.position + new Vector3(0.0f, 1.1f, 0.0f), -transform.up, out hit2, 1.6f, SpotLayer))
+                FindSpot(hit2, Checkingsort.falling);
+        }
+    }
+
+    public void CheckForPlateau() {
+        RaycastHit hit2;
+
+        Vector3 dir = transform.up + transform.forward / 2.0f;
+
+        if (!Physics.Raycast(HandTransform.position + transform.rotation * VerticalHandOffset, dir, out hit2, 1.5f, SpotLayer)) {
+            currentSort = Climbingsort.ClimbingTowardsPlateau;
+
+            if (Physics.Raycast(HandTransform.position + dir * 1.5f, -Vector3.up, out hit2, 1.7f, SpotLayer))
+                TargetPoint = HandTransform.position + dir * 1.5f;
+            else
+                TargetPoint = HandTransform.position + dir * 1.5f - transform.rotation * new Vector3(0, -0.2f, 0.25f);
+
+            TargetNormal = -transform.forward;
+
+            anim.SetBool("Crouch", true);
+            anim.SetBool("OnGround", true);
+        }
+    }
+
+    [System.Serializable]
+    public enum Climbingsort {
+        Walking,
+        Jumping,
+        Falling,
+        Climbing,
+        ClimbingTowardsPoint,
+        ClimbingTowardsPlateau,
+        CheckingForClimbStart
+    }
+
+    [System.Serializable]
+    public class RayInfo {
+        public Vector3 point;
+        public Vector3 normal;
+        public bool CanGoToPoint;
+    }
+
+    [System.Serializable]
+    public enum Checkingsort
+    {
+        normal,
+        turning,
+        falling
+    }
+}
